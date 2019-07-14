6f5abe48ac631c95465f176b930a10d83fc97abc
ungverd <cfvhfyljvytav@mail.ru>
2019-07-14 11:37:15 +0300

    update shadoweditor.py

--- shadoweditor.py
+++ shadoweditor.py
@@ -17,7 +17,89 @@ from pydub.exceptions import CouldntDecodeError
 
 FRAMERATE = 48000
 SAMPLEWIDTH = 2 #2 bytes == 16 bits
-DST = os.getcwd()
+
+master = Tk()
+master.geometry('500x600')
+tree=ttk.Treeview(master)
+
+class WindowState:
+    initWidgets = []
+    nowWidgets = []
+    
+    def begin(self):
+        for widget in self.initWidgets:
+            widget.pack()
+        self.nowWidgets += self.initWidgets
+        
+    def end(self):
+        for widget in self.nowWidgets:
+            widget.pack_forget()
+        self.nowWidgets = []
+            
+    def add(self, widget):
+        widget.pack()
+        self.nowWidgets.append(widget)
+
+    def addTop(self, widget):
+        widget.pack()
+        self.nowWidgets = [widget] + self.nowWidgets
+    
+
+class EnterDest(WindowState):
+    def __init__(self):
+        self.entryForDest = Entry(master)
+        self.entryForDest.bind('<Return>', firstCallback)
+        self.selectDestB = Button(master, text="Выбрать папку для записи музыки", width=30, command=selectDestFolder)
+        self.enterDestLabel = Label(master, text="\nИЛИ\nВведите путь к папке", justify=CENTER)
+        self.notValidDestLabel = Label(master, text="Некорректный путь", foreground='#EE0000')
+        self.initWidgets = [self.selectDestB, self.enterDestLabel, self.entryForDest]
+
+    def begin(self):
+        WindowState.begin(self)
+        self.entryForDest.focus_set()
+
+class EnterSource(WindowState):
+    def __init__(self):
+        self.entryForFolder = Entry(master)
+        self.entryForFolder.bind('<Return>', callback)
+        self.selectFolderB = Button(master, text="Выбрать исходную папку с музыкой", width=30, command=selectFolder)
+        self.enterPathLabel = Label(master, text="\nИЛИ\nВведите путь к папке", justify=CENTER)
+        self.notValidPathLabel = Label(master, text="Некорректный путь", foreground='#EE0000')
+        self.tree = tree
+        self.initWidgets = [self.selectFolderB, self.enterPathLabel, self.entryForFolder]
+
+    def begin(self):
+        WindowState.begin(self)
+        self.entryForFolder.focus_set()
+
+    def end(self):
+        WindowState.end(self)
+        self.entryForFolder.delete(0, 'end')
+
+    def clearField(self):
+        self.entryForFolder.delete(0, 'end')
+
+class WriteMusic(WindowState):
+    def __init__(self):
+        self.tree = tree
+        self.initWidgets = [tree]
+
+class ConvertCopy(WindowState):
+    def __init__(self):
+        self.currentfolder=ttk.Treeview(master)
+        self.currentfolder.tag_configure('red', foreground='#EE0000')
+        self.currentfolder.tag_configure('grey', foreground='#888888')
+        self.convB = Button(master, text="конвертировать", width=15, command=lambda : convertOrCopy(convert))
+        self.copyB = Button(master, text="копировать только подходящие", width=30, command=lambda : convertOrCopy(copyOnly))
+        self.backB = Button(master, text="назад", width=10, command=back)
+        self.convertOrCopyLabel = Label(master, text="Есть файлы в неподходящем формате (выделены красным). Конвертировать?")
+        self.initWidgets = [self.backB, self.currentfolder]
+
+    def end(self):
+        WindowState.end(self)
+        self.currentfolder.delete(*self.currentfolder.get_children())
+    
+
 
 class FileFormat(Enum):
     mp3 = 1
@@ -58,8 +140,8 @@ def convert(src, dst):
             sound = sound.set_sample_width(SAMPLEWIDTH)
             sound.export(dst, format="wav")
             return True
-        except CouldntDecodeError:
-            pass
+        except CouldntDecodeError as e:
+            print('Error during processing file %s: %s' % (src, e))
     elif res == FileFormat.goodWav:
         if src != dst:
             copyfile(src, dst)
@@ -75,82 +157,75 @@ def copyOnly(src, dst):
 def convertOrCopy(func):
     src_path = master.path
     basename = os.path.basename(src_path)
-    os.mkdir(basename)
-    folder = tree.insert("", 1, "", text=basename)
+    full_dest = os.path.join(master.dest, basename)
+    if not os.path.isdir(full_dest):
+        os.mkdir(full_dest)
+    else:
+        i = 1
+        new_name = full_dest + str(i)
+        while os.path.isdir(new_name):
+            i += 1
+            new_name = full_dest + str(i)
+        os.mkdir(new_name)
+        full_dest = new_name
+        basename = os.path.basename(full_dest)
+    folder = enterSourceObj.tree.insert("", 1, "", text=basename)
     for filename in os.listdir(src_path):
         src = os.path.join(src_path, filename)
-        dst = os.path.join(DST, basename, filename)
+        dst = os.path.join(full_dest, filename)
         if func(src, dst):
-            tree.insert(folder, "end", "", text=filename[:-4] + ".wav")
-    convB.pack_forget()
-    copyB.pack_forget()
-    backB.pack_forget()
-    tree.pack()
-    currentfolder.pack_forget()
-    convertOrCopyLabel.pack_forget()
-    selectFolderB.pack()
-    enterPathLabel.pack()
-    entryForFolder.pack()
-    entryForFolder.focus_set()
+            enterSourceObj.tree.insert(folder, "end", "", text=filename[:-4] + ".wav")
+    
+    convertCopyObj.end()
+    enterSourceObj.begin()
+    enterSourceObj.add(enterSourceObj.tree)
 
 def back():
-    convB.pack_forget()
-    copyB.pack_forget()
-    backB.pack_forget()
-    currentfolder.pack_forget()
-    convertOrCopyLabel.pack_forget()
-    selectFolderB.pack()
-    enterPathLabel.pack()
-    entryForFolder.pack()
-    entryForFolder.focus_set()
+    convertCopyObj.end()
+    enterSourceObj.begin()
     
 def callback(event):
-    master.path = entryForFolder.get()
+    master.path = enterSourceObj.entryForFolder.get()
     doAfterEnterPath()
 
 def firstCallback(event):
-    dest = entryForDest.get()
+    dest = enterDestObj.entryForDest.get()
     if os.path.isdir(dest):
         master.dest = dest
         doAfterSelectDest()
     else:
-        entryForDest.delete(0, 'end')
-        notValidDestLabel.pack()
-        InitialWindow.append(notValidDestLabel)
+        enterDestObj.entryForDest.delete(0, 'end')
+        enterDestObj.add(enterDestObj.notValidDestLabel)
 
 def doAfterEnterPath():
-    notValidPathLabel.pack_forget()
-    ChoiceWindow = [backB, currentfolder]
     haveToConvert = False
     haveToCopy = False
-    currentfolder.delete(*currentfolder.get_children())
     try:
         for filename in os.listdir(master.path):
             src = os.path.join(master.path, filename)
             res = classifyFile(src)
             if res == FileFormat.goodWav:
-                currentfolder.insert("", 1, "", text=filename)
+                convertCopyObj.currentfolder.insert("", 1, "", text=filename)
                 haveToCopy = True
             elif res == FileFormat.notMusic:
-                currentfolder.insert("", 1, "", text=filename, tags = ('grey',))
+                convertCopyObj.currentfolder.insert("", 1, "", text=filename, tags = ('grey',))
             else:
-                currentfolder.insert("", 1, "", text=filename, tags = ('red',))
+                convertCopyObj.currentfolder.insert("", 1, "", text=filename, tags = ('red',))
                 haveToConvert = True
+
+        enterSourceObj.end()
                 
         if haveToConvert:
-            ChoiceWindow = [convertOrCopyLabel, convB] + ChoiceWindow
+            convertCopyObj.addTop(convertCopyObj.convertOrCopyLabel)
+            convertCopyObj.addTop(convertCopyObj.convB)
         if haveToCopy:
-            ChoiceWindow = [copyB] + ChoiceWindow
+            convertCopyObj.addTop(convertCopyObj.copyB)
             
-        for widget in SelectFolderWindow:
-            widget.pack_forget()
-        for widget in ChoiceWindow:
-            widget.pack()
+        convertCopyObj.begin()
 
     except (FileNotFoundError, OSError):
-        notValidPathLabel.pack()
-        SelectFolderWindow.append(notValidPathLabel)
-    entryForFolder.delete(0, 'end')
+        enterSourceObj.add(enterSourceObj.notValidPathLabel)
+        enterSourceObj.clearField()
 
 def selectFolder():
     master.path =  filedialog.askdirectory(initialdir = "/")
@@ -178,53 +253,17 @@ def selectDestFolder():
     doAfterSelectDest()
 
 def doAfterSelectDest():
-    for widget in InitialWindow:
-        widget.pack_forget()
-    for widget in SelectFolderWindow:
-        widget.pack()
-    entryForFolder.focus_set()
+    enterDestObj.end()
+    enterSourceObj.begin()
     
 
 ######################################
 
-master = Tk()
-master.geometry('500x600')
+enterDestObj = EnterDest()
+enterSourceObj = EnterSource()
+writeMusicObj = WriteMusic()
+convertCopyObj = ConvertCopy()
 
 
-currentfolder=ttk.Treeview(master)
-currentfolder.tag_configure('red', foreground='#EE0000')
-currentfolder.tag_configure('grey', foreground='#888888')
-
-entryForFolder = Entry(master)
-entryForFolder.bind('<Return>', callback)
-selectFolderB = Button(master, text="Выбрать папку", width=15, command=selectFolder)
-enterPathLabel = Label(master, text="\nИЛИ\nВведите путь к папке", justify=CENTER)
-SelectFolderWindow = [selectFolderB, enterPathLabel, entryForFolder]
-
-notValidPathLabel = Label(master, text="Некорректный путь", foreground='#EE0000')
-convertOrCopyLabel = Label(master, text="Есть звуковые файлы в неподходящем формате, конвертировать?")
-
-entryForDest = Entry(master)
-entryForDest.bind('<Return>', firstCallback)
-selectDestB = Button(master, text="Выбрать папку для записи музыки", width=30, command=selectDestFolder)
-enterDestLabel = Label(master, text="\nИЛИ\nВведите путь к папке", justify=CENTER)
-notValidDestLabel = Label(master, text="Некорректный путь", foreground='#EE0000')
-InitialWindow = [selectDestB, enterDestLabel, entryForDest]
-
-
-tree=ttk.Treeview(master)
-
-convB = Button(master, text="конвертировать", width=15, command=lambda : convertOrCopy(convert))
-copyB = Button(master, text="копировать только подходящие", width=30, command=lambda : convertOrCopy(copyOnly))
-backB = Button(master, text="назад", width=10, command=back)
-ChoiceWindow = [backB, currentfolder]
-
-WriteWindow = [tree]
-
-########################################
-
-for widget in InitialWindow:
-    widget.pack()
-entryForDest.focus_set()
-
+enterDestObj.begin()
 mainloop()
